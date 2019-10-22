9e32c886e27ba51662c74d6996bfd2ea35fc7d4a
bobby-he <bobbyhe@gotmail.co.uk>
2019-10-22 23:11:13 +0100

    ok

--- jl_iBatchLearn.py
+++ jl_iBatchLearn.py
@@ -118,7 +118,7 @@ def get_args(argv):
     parser.add_argument('--lr', type=float, default=0.01, help="Learning rate")
     parser.add_argument('--momentum', type=float, default=0)
     parser.add_argument('--weight_decay', type=float, default=0)
-    parser.add_argument('--schedule', nargs="+", type=int, default=[15],
+    parser.add_argument('--schedule', nargs="+", type=int, default=[50],
                         help="The list of epoch numbers to reduce learning rate by factor of 0.1. Last number is the end epoch")
     parser.add_argument('--print_freq', type=float, default=100, help="Print the log at every x iteration")
     parser.add_argument('--model_weights', type=str, default=None,
