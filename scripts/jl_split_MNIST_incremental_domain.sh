18059790d8c6539870866a835e9176b15f220574
bobby-he <bobbyhe@gotmail.co.uk>
2019-10-31 23:02:23 +0000

    ok

--- scripts/jl_split_MNIST_incremental_domain.sh
+++ scripts/jl_split_MNIST_incremental_domain.sh
@@ -2,5 +2,5 @@ GPUID=$1
 OUTDIR=outputs/split_MNIST_incremental_domain
 REPEAT=1
 mkdir -p $OUTDIR
-python3 -u jl_iBatchLearn.py --gpuid $GPUID --repeat $REPEAT --reg_coef 0.05 --damping 0.05 | tee ${OUTDIR}/jl_test_4.log
+python3 -u jl_iBatchLearn.py --gpuid $GPUID --repeat $REPEAT --reg_coef 1 --damping 0.05 | tee ${OUTDIR}/jl_test_4.log
 
